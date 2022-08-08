2be20b8033adb277978d6ce7d304c70d22048eff
Bob Jackson <1187688895@qq.com>
2022-08-08 22:10:15 +0800

    doc

--- src/main/java/csproblem/injava/chapter1/PiCalculator.java
+++ src/main/java/csproblem/injava/chapter1/PiCalculator.java
@@ -1,5 +1,9 @@
 package csproblem.injava.chapter1;
 
+/*
+ * Leibniz formula
+ * π = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11...
+ */
 public class PiCalculator {
 
     public static double calculate(int nTerms) {
