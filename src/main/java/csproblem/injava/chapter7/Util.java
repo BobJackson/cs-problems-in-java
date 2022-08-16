bc5e8299fa1b4981c124a27d729626b31a68b5af
Bob Jackson <1187688895@qq.com>
2022-08-16 21:54:34 +0800

    add Util

---
+++ src/main/java/csproblem/injava/chapter7/Util.java
@@ -0,0 +1,21 @@
+package csproblem.injava.chapter7;
+
+public final class Util {
+
+    public static double dotProduct(double[] xs, double[] ys) {
+        double sum = 0.0;
+        for (int i = 0; i < xs.length; i++) {
+            sum += (xs[i] * ys[i]);
+        }
+        return sum;
+    }
+
+    public static double sigmoid(double x) {
+        return 1.0 / (1.0 + Math.exp(-x));
+    }
+
+    public static double derivativeSigmoid(double x) {
+        double sig = sigmoid(x);
+        return sig * (1.0 - sig);
+    }
+}
