28746c39ce71b5db11ed98e330a7af9cd1114e25
Bob Jackson <1187688895@qq.com>
2022-08-16 22:47:04 +0800

    update Util

--- src/main/java/csproblem/injava/chapter7/Util.java
+++ src/main/java/csproblem/injava/chapter7/Util.java
@@ -1,5 +1,15 @@
 package csproblem.injava.chapter7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public final class Util {
 
     public static double dotProduct(double[] xs, double[] ys) {
@@ -17,5 +27,35 @@ public final class Util {
     public static double derivativeSigmoid(double x) {
         double sig = sigmoid(x);
         return sig * (1.0 - sig);
+    }
+
+    public static void normalizeByFeatureScaling(List<double[]> dataSet) {
+        for (int colNum = 0; colNum < dataSet.get(0).length; colNum++) {
+            List<Double> column = new ArrayList<>();
+            for (double[] row : dataSet) {
+                column.add(row[colNum]);
+            }
+            double maximum = Collections.max(column);
+            double minimum = Collections.min(column);
+            double difference = maximum - minimum;
+            for (double[] row : dataSet) {
+                row[colNum] = (row[colNum] - minimum) / difference;
+            }
+        }
+    }
+
+    public static List<String[]> loadCSV(String filename) {
+        try (InputStream inputStream = Util.class.getResourceAsStream(filename)) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            return bufferedReader.lines().map(line -> line.split(",")).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public static double max(double[] numbers) {
+        return Arrays.stream(numbers).max().orElse(Double.MIN_NORMAL);
     }
 }
