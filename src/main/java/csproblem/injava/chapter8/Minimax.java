9b69f4f4bc525f100be4365919b05671e5e0036a
Bob Jackson <1187688895@qq.com>
2022-08-19 01:06:53 +0800

    add alpha-beta pruning

--- src/main/java/csproblem/injava/chapter8/Minimax.java
+++ src/main/java/csproblem/injava/chapter8/Minimax.java
@@ -2,7 +2,20 @@ package csproblem.injava.chapter8;
 
 public class Minimax {
 
-    public static <Move> double minimax(Board<Move> board, boolean maximizing, Piece originalPlayer, int maxDepth) {
+    public static <Move> Move findBestMove(Board<Move> board, int maxDepth) {
+        double bestEval = Double.NEGATIVE_INFINITY;
+        Move bestMove = null;
+        for (Move move : board.getLegalMoves()) {
+            double result = minimax(board.move(move), false, board.getTurn(), maxDepth);
+            if (result > bestEval) {
+                bestEval = result;
+                bestMove = move;
+            }
+        }
+        return bestMove;
+    }
+
+    private static <Move> double minimax(Board<Move> board, boolean maximizing, Piece originalPlayer, int maxDepth) {
         if (board.isWin() || board.isDraw() || maxDepth == 0) {
             return board.evaluate(originalPlayer);
         }
@@ -23,16 +36,38 @@ public class Minimax {
         }
     }
 
-    public static <Move> Move findBestMove(Board<Move> board, int maxDepth) {
-        double bestEval = Double.NEGATIVE_INFINITY;
-        Move bestMove = null;
-        for (Move move : board.getLegalMoves()) {
-            double result = minimax(board.move(move), false, board.getTurn(), maxDepth);
-            if (result > bestEval) {
-                bestEval = result;
-                bestMove = move;
-            }
+    private static <Move> double alphabeta(Board<Move> board, boolean maximizing, Piece originalPlayer, int maxDepth) {
+        return alphabeta(board, maximizing, originalPlayer, maxDepth, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+
+    private static <Move> double alphabeta(Board<Move> board,
+                                           boolean maximizing,
+                                           Piece originalPlayer,
+                                           int maxDepth,
+                                           double alpha,
+                                           double beta) {
+        if (board.isWin() || board.isDraw() || maxDepth == 0) {
+            return board.evaluate(originalPlayer);
         }
-        return bestMove;
+
+        if (maximizing) {
+            for (Move m : board.getLegalMoves()) {
+                alpha = Math.max(alpha,
+                        alphabeta(board.move(m), false, originalPlayer, maxDepth - 1, alpha, beta));
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return alpha;
+        } else {
+            for (Move m : board.getLegalMoves()) {
+                beta = Math.min(beta,
+                        alphabeta(board.move(m), true, originalPlayer, maxDepth - 1, alpha, beta));
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return beta;
+        }
     }
 }
